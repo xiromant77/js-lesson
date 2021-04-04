4dda0918321acc6f4d3dc5cbee1edcc67f5f4e38
yehor <xiromant77@gmail.com>
2021-04-04 22:00:52 +0300

    Lesson 10. IF ELSE SWITCH CASE

--- script.js
+++ script.js
@@ -4,6 +4,41 @@
 
 
 
+
+// Урок 10. Условия
+
+
+
+
+
+// if (num < 49) {
+//     console.log ('Error');
+// }
+//     else if (num > 100) {
+//         console.log("To much");
+//     }
+//     else {
+//         console.log("Ok");
+//     }
+//  (num === 50) ? console.log("Ok!") : console.log('Error!');
+
+//  const num = 49;
+
+//  switch(num) {
+//      case 49:
+//          console.log("Wrong");
+//          break;
+//     case 100:
+//          console.log("Wrong");
+//          break;
+//     case 50:
+//          console.log("YES");
+//          break;      
+//     default:
+//         console.log("Not in this time");
+//         break;         
+//  }
+
 //Урок 7. Git 
 
 // Працюючи з файлом - 
